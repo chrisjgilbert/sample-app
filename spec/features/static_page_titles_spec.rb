a97ad198926a8fda1e7b24430cb832790c65b200
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-05 16:40:47 +0000

    routes updated so static routes are now named routes

--- spec/features/static_page_titles_spec.rb
+++ spec/features/static_page_titles_spec.rb
@@ -4,28 +4,28 @@ RSpec.describe 'static page titles', :type => :feature do
   base_title = "Ruby on Rails Tutorial Sample App"
   describe 'home' do
     it "has `home` as title" do
-      visit static_pages_home_path
+      visit root_path
       expect(page.title).to eq "#{base_title}"
     end
   end
 
   describe 'about' do
     it "has `about` as title" do
-      visit static_pages_about_path
+      visit about_path
       expect(page.title).to eq "about | #{base_title}"
     end
   end
 
   describe 'help' do
     it "has `help` as title" do
-      visit static_pages_help_path
+      visit help_path
       expect(page.title).to eq "help | #{base_title}"
     end
   end
 
   describe 'contact' do
     it "has `help` as title" do
-      visit static_pages_contact_path
+      visit contact_path
       expect(page.title).to eq "contact | #{base_title}"
     end
   end
