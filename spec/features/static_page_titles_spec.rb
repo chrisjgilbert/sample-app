000c393bf9d11e6577a2cbde4bc7e245dc172e50
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-06 16:33:34 +0000

    updated feature tests to click on links and check they are correct

--- spec/features/static_page_titles_spec.rb
+++ spec/features/static_page_titles_spec.rb
@@ -2,30 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'static page titles', :type => :feature do
   base_title = "Ruby on Rails Tutorial Sample App"
+  before do
+    visit root_path
+  end
   describe 'home' do
     it "has `home` as title" do
-      visit root_path
+      click_on "Home"
+      expect(current_path).to eq root_path
       expect(page.title).to eq "#{base_title}"
     end
   end
 
   describe 'about' do
     it "has `about` as title" do
-      visit about_path
+      click_on "About"
+      expect(current_path).to eq about_path
       expect(page.title).to eq "about | #{base_title}"
     end
   end
 
   describe 'help' do
     it "has `help` as title" do
-      visit help_path
+      click_on "Help"
+      expect(current_path).to eq help_path
       expect(page.title).to eq "help | #{base_title}"
     end
   end
 
   describe 'contact' do
     it "has `help` as title" do
-      visit contact_path
+      click_on "Contact"
+      expect(current_path).to eq contact_path
       expect(page.title).to eq "contact | #{base_title}"
     end
   end
