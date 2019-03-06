3c1ff64b0af57c8765fb02fece46804ae5d724ea
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-06 17:51:08 +0000

    added length validations to users name and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,20 @@ RSpec.describe User, type: :model do
     before do
       @user = User.new(name: 'Example User', email: 'user@example.com')
     end
-    it "should be valid" do
+    it "is valid when attributes are valid" do
       expect(@user).to be_valid
     end
-    it "should not be valid if name and email are blank" do
+    it "is not valid if name and email are blank" do
       @user.name = "   "
       @user.email = "   "
+      expect(@user).not_to be_valid
+    end
+    it "is not valid if name is above 50 chars" do
+      @user.name = "a" * 51
+      expect(@user).not_to be_valid
+    end
+    it "is not valid if email is above 255 chars" do
+      @user.email = "user" + "@" * 241 + "example.com"
       expect(@user).not_to be_valid
     end
   end
