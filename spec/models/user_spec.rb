cf526f57b78df786e39f79f01d1693e3da663e78
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-06 19:05:48 +0000

    user email regex validation added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,21 +5,39 @@ RSpec.describe User, type: :model do
     before do
       @user = User.new(name: 'Example User', email: 'user@example.com')
     end
-    it "is valid when attributes are valid" do
-      expect(@user).to be_valid
+    context 'name' do
+      it "is valid when attributes are valid" do
+        expect(@user).to be_valid
+      end
+      it "is not valid if name and email are blank" do
+        @user.name = "   "
+        @user.email = "   "
+        expect(@user).not_to be_valid
+      end
+      it "is not valid if name is above 50 chars" do
+        @user.name = "a" * 51
+        expect(@user).not_to be_valid
+      end
     end
-    it "is not valid if name and email are blank" do
-      @user.name = "   "
-      @user.email = "   "
-      expect(@user).not_to be_valid
-    end
-    it "is not valid if name is above 50 chars" do
-      @user.name = "a" * 51
-      expect(@user).not_to be_valid
-    end
-    it "is not valid if email is above 255 chars" do
-      @user.email = "user" + "@" * 241 + "example.com"
-      expect(@user).not_to be_valid
+    context 'email' do
+      it "is not valid if email is above 255 chars" do
+        @user.email = "user" + "@" * 241 + "example.com"
+        expect(@user).not_to be_valid
+      end
+      it "accepts valid addresses" do
+        valid_addresses = %w[user@example.com chris@EMAIL.com TEST@email.co.uk]
+        valid_addresses.each do |valid_address|
+          @user.email = valid_address
+          expect(@user).to be_valid
+        end
+      end
+      it "rejects invalid email addresses" do
+        invalid_addresses = %w[user@example,com user_at_foo.org user@example.]
+        invalid_addresses.each do |invalid_address|
+          @user.email = invalid_address
+          expect(@user).not_to be_valid
+        end
+      end
     end
   end
 end
