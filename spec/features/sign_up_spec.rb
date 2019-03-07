1292fbf84b239d2c1b596dea0ee0dce6a991c453
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-07 15:43:22 +0000

    added redirect to users page afer successful form submission

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "user sign up", :type => :feature do
-  it "wont allow a user with invalid attributes to sign up" do
+  before do
     visit new_user_path
+  end
+  it "wont allow a user with invalid attributes to sign up" do
     fill_in 'user[name]', with: '   '
     fill_in 'user[email]', with: 'jon@b'
     fill_in 'user[password]', with: 'a'
     fill_in 'user[password_confirmation]', with: 'a'
     click_on 'Create my account'
     expect(page).to have_content('3 errors')
+    expect(current_path).to eq sign_up_path
+  end
+
+  it "will allow a user with valid attributes to sign up" do
+    user = User.new(name: 'Test User',
+                    email: 'test@user.com',
+                    password: 'secret',
+                    password_confirmation: 'secret')
+    fill_in 'user[name]', with: user.name
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password
+    click_on 'Create my account'
+    expect(page).not_to have_content('errors')
+    expect(current_path).to eq "/users/1"
   end
 end
