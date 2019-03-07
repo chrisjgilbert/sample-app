e6e7e15ec6a9a0499016fbeee1d66742e04ce1b7
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-07 16:14:54 +0000

    corrected error in puma config gile

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,6 @@ rackup DefaultRackup
 port ENV['PORT']            || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_work_boot do
+on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
