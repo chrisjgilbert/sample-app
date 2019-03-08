86b591caa83f2fa78d2d1ec5224b8e41e488aaba
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-08 15:59:37 +0000

    added remember_digest to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_104640) do
+ActiveRecord::Schema.define(version: 2019_03_08_155521) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
