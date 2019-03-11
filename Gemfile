323939626cb0cee186843cb9a3fa1d77d281917f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-11 12:40:25 +0000

    added in users#index action with pagination

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'rails', '~> 5.2.2'
 gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
+gem 'faker'
+gem 'will_paginate', '3.1.5'
+gem 'bootstrap-will_paginate'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+  describe 'GET #index' do
+    subject { get :index }
+    context 'when not logged in' do
+      it 'redirects to login url' do
+        expect(subject).to redirect_to login_url
+      end
+    end
+    context 'when logged in' do
+      it 'displays all users' do
+        allow(controller).to receive(:logged_in_user) { true }
+        expect(subject).to render_template(:index)
+      end
+    end
+  end
 end
