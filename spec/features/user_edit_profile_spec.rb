d88337568c1e9c19dd8797dde2bf375493ed5127
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-11 11:36:30 +0000

    added in edit user redirect when not logged in

--- spec/features/user_edit_profile_spec.rb
+++ spec/features/user_edit_profile_spec.rb
@@ -2,33 +2,43 @@
 
 require 'rails_helper'
 
-RSpec.describe 'user log in', type: :feature do
+RSpec.describe 'edit user profile', type: :feature do
   before do
     @user = User.create(name: 'test',
                         email: 'test@example.com',
                         password: 'secret',
                         password_confirmation: 'secret')
-    visit login_path
-    login(@user)
-    click_on 'Account'
-    click_on 'Settings'
   end
-  context 'unsuccessful' do
-    it 'flashes error message' do
-      click_on 'Save changes'
-      expect(page).not_to have_content 'Profile updated!'
-      expect(page).to have_content 'The form contains 2 errors'
+  context 'when user is logged in' do
+    before do
+      visit login_path
+      login(@user)
+      click_on 'Account'
+      click_on 'Settings'
+    end
+    context 'unsuccessful' do
+      it 'flashes error message' do
+        click_on 'Save changes'
+        expect(page).not_to have_content 'Profile updated!'
+        expect(page).to have_content 'The form contains 2 errors'
+      end
+    end
+    context 'successful' do
+      it 'logs user in' do
+        fill_in 'user[name]', with: 'edited name'
+        fill_in 'user[email]', with: 'edited@email.com'
+        fill_in 'user[password]', with: 'secret'
+        fill_in 'user[password_confirmation]', with: 'secret'
+        click_on 'Save changes'
+        expect(page).to have_content 'Profile updated!'
+        expect(page).not_to have_content '3 errors'
+      end
     end
   end
-  context 'successful' do
-    it 'logs user in' do
-      fill_in 'user[name]', with: 'edited name'
-      fill_in 'user[email]', with: 'edited@email.com'
-      fill_in 'user[password]', with: 'secret'
-      fill_in 'user[password_confirmation]', with: 'secret'
-      click_on 'Save changes'
-      expect(page).to have_content 'Profile updated!'
-      expect(page).not_to have_content '3 errors'
+  context 'when user is not logged in' do
+    it 'is redirected to the login path' do
+      visit edit_user_path(@user)
+      expect(current_path).to eq login_path
     end
   end
 end
