41f46015475b8b2908009f7457ca70dc24bd7876
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-12 19:58:43 +0000

    added password reset controller and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -15,18 +15,14 @@ class User < ApplicationRecord
                        length: { minimum: 6 }
   has_secure_password
 
-  class << self
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ?
+    BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-    def digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ?
-      BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
-
-    def new_token
-      SecureRandom.urlsafe_base64
-    end
-
+  def User.new_token
+    SecureRandom.urlsafe_base64
   end
 
   def remember
@@ -51,6 +47,16 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
