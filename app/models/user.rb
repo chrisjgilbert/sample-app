c218b0685c3fe2e9f631d9e9b90a027785402990
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-13 10:14:04 +0000

    password reset implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,14 +49,22 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
-
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
+  end
+
+  def clear_reset_token
+    update_columns(reset_digest: nil, reset_sent_at: nil)
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
