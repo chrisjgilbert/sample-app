77ec37f6f31061d4061e4187990a8b1e3cc952dc
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-13 16:53:24 +0000

    added microposts and feed implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,14 +56,6 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in'
-      redirect_to login_url
-    end
-  end
-
   def set_user
     @user = User.find(params[:id])
   end
